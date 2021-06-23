41263c06fac2cb9530023fcf63e037f00b64a974
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-23 20:59:38 -0300

    Configuração do server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const server = express()
+
+const route = require('./route')
+
+server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, './views'))
+
+server.use(express.static('public'))
+server.use(route)
+
+server.listen(3000, () => console.log('Server Online'))
+
