1b70bc450993921f972aca98bb677e586d2e1666
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-23 21:23:54 -0300

    rotas

--- src/route.js
+++ src/route.js
@@ -2,8 +2,9 @@ const express = require('express')
 
 const route = express.Router()
 
-route.get('/', (req,res) => {
-    res.render('index')
-})
+route.get('/', (req,res) => res.render('index') )
+route.get('/create', (req,res) => res.render('create') )
+route.get('/room', (req,res) => res.render('room') )
+
 
 module.exports = route
