909544251b4a11ff80eff45c0dd477f162609df5
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-24 00:01:05 -0300

    configuração para o post

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ server.set('view engine', 'ejs')
 server.set('views', path.join(__dirname, './views'))
 
 server.use(express.static('public'))
+server.use(express.urlencoded({extended: true}))
 server.use(route)
 
 server.listen(3000, () => console.log('Server Online'))
