d35e5f7ed372eb1c8ff315f4e818f2b9d32dae73
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-24 12:52:10 -0300

    implementação dos controllers

--- src/route.js
+++ src/route.js
@@ -1,11 +1,17 @@
+const { render } = require('ejs')
 const express = require('express')
-
+const QuestionController = require('./controllers/Question')
+const RoomController = require('./controllers/Room')
 const route = express.Router()
 
 // Gets
 route.get('/', (req,res) => res.render('index',{page: 'enter-room'}))
 route.get('/create', (req,res) => res.render('index', {page: 'create'} ))
-route.get('/room', (req,res) => res.render('room') )
+route.get('/room/:room', (req,res) => res.render('room') )
 
+//  Posts
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
+route.post('/room/', (req,res) => res.redirect(`/room/${req.body.rooms}`))
 
 module.exports = route
