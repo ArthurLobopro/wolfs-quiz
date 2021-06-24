c5d17dbf32ddc43afd1b8328964e66918beb1a23
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-24 13:03:58 -0300

    Funções do controle da sala

--- src/controllers/Room.js
+++ src/controllers/Room.js
@@ -1,6 +1,25 @@
+const Database = require('../db/config')
 module.exports = {
-    create(req,res){
-        let room = '123456'
+    async create(req,res){
+        const db = await Database()
+        const password = req.body.password
+        let room = ''
+        for(let i =0;i < 6; i++){
+            room+= Math.floor(Math.random() * 10)
+        }
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+            ) VALUES (
+                ${room},
+                ${password}
+            )`)
+
+        await db.close()
+
+        // console.table({room,password})
+
         res.redirect(`/room/${room}`)
     }
 }
