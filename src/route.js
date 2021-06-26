61720cc12e9626caf2abe61e50ef16072b37d258
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-25 22:24:20 -0300

    Update route.js

--- src/route.js
+++ src/route.js
@@ -7,11 +7,12 @@ const route = express.Router()
 // Gets
 route.get('/', (req,res) => res.render('index',{page: 'enter-room'}))
 route.get('/create', (req,res) => res.render('index', {page: 'create'} ))
-route.get('/room/:room', (req,res) => res.render('room') )
+route.get('/room/:room', RoomController.open )
 
 //  Posts
 route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/room/', (req,res) => res.redirect(`/room/${req.body.room}`))
 route.post('/create-room', RoomController.create)
-route.post('/room/', (req,res) => res.redirect(`/room/${req.body.rooms}`))
+route.post('/question/create/:room', QuestionController.create)
 
 module.exports = route
