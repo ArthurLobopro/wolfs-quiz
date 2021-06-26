521d8d88a20201becbd20ec653f9705589a39617
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-26 00:14:14 -0300

    controle das questões

--- src/controllers/Question.js
+++ src/controllers/Question.js
@@ -1,8 +1,53 @@
-module.exports = {
-    index(req,res){
+const Database = require('../db/config')
+
+const QuestionController = {
+    async index(req,res){
         const { room, question, action } = req.params
         const password = req.body.password
+
+        // Verificação da senha
+        const db = await Database()
+        const dbPassword = await db.get(`SELECT * FROM rooms WHERE id = ${room}`)
+        
+        if(dbPassword.pass === password){
+            if(action === 'check'){
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${question}`)
+            }
+
+            if(action === 'delete'){
+                db.run(`DELETE FROM questions WHERE id = ${question}`)
+            }
+
+            return res.redirect(`/room/${room}`)
+        }
+
+        res.render('pass-error', { room })
         console.log({ room, question, action });
-        console.log(password);
+        // console.log(password);
+    },
+    async create(req,res){
+        const question = req.body.question
+        const room = req.params.room
+        const db = await Database()
+
+        await db.run(`INSERT INTO questions (
+            title,
+            read,
+            room
+        ) VALUES (
+            "${question}",
+            0,
+            "${room}"
+        )`)
+
+        await db.close()
+
+        res.redirect(`/room/${room}`)
+    },
+    async delete(){
+        const db = await Database()
+        // const roomPassword = await 
     }
 }
+
+module.exports = QuestionController
