bae02171835431b7e671f32848085d7b6a46b02c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-25 22:23:56 -0300

    geração randomica de salas melhorada para evitar ids repetidos e função de abrir sala

--- src/controllers/Room.js
+++ src/controllers/Room.js
@@ -1,13 +1,27 @@
 const Database = require('../db/config')
-module.exports = {
+
+const RoomController = {
     async create(req,res){
         const db = await Database()
         const password = req.body.password
         let room = ''
-        for(let i =0;i < 6; i++){
-            room+= Math.floor(Math.random() * 10)
+        
+        while(true){
+            for(let i =0;i < 6; i++){
+                room+= Math.floor(Math.random() * 10)
+            }
+        
+            //Verificação se a sala já existe
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+        
+            if(!roomsExistIds.indexOf(room) !== -1){
+                break
+            }else{
+                room = ''
+            }
         }
-
+    
+        //Inserir sala no banco de dados
         await db.run(`INSERT INTO rooms (
             id,
             pass
@@ -15,11 +29,23 @@ module.exports = {
                 ${room},
                 ${password}
             )`)
+    
+        await db.close()
+    
+        // console.table({room,password})
+    
+        res.redirect(`/room/${room}`)
+    },
+    async open(req,res){
+        const roomId = req.params.room
+        const db = await Database()
 
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const readQuestions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
         await db.close()
 
-        // console.table({room,password})
-
-        res.redirect(`/room/${room}`)
+        res.render('room', { roomId, questions, readQuestions, isVoid: questions.length === 0 && readQuestions.length === 0 })
     }
 }
+
+module.exports = RoomController
